813f3287f3ef743f9b4f2a2a4356633f7fea1ada
douglasbcosta <douglasb.costa@hotmail.com>
2019-05-24 19:30:48 -0300

    Criação do Projeto

---
+++ DouglasStore.Shared/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DouglasStore.Shared
+{
+    public class Class1
+    {
+    }
+}
---
+++ DouglasStore.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DouglasStore.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
